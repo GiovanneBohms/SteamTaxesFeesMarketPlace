c7930752cd6e3d5c0c0b7a36cb1bd0d84b22160f
Giovanne <giovannebohms@gmail.com>
2024-07-02 23:43:03 -0300

    init

--- rateCalculator.js
+++ rateCalculator.js
@@ -1,53 +1,54 @@
-function subtraiTaxa(entrada){
-    let intervalos = [0.02,0.21,0.32,0.43]
-    let taxas =[0.02,0.03,0.04,0.05,0.07,0.09]
-  
-      while (entrada >intervalos[intervalos.length -1]){
-        const ultimoElemento = intervalos[intervalos.length -1]
-        if (intervalos.length % 2 === 0) {
-         intervalos.push(parseFloat((ultimoElemento+0.12).toFixed(2)))
-        } else {
-          intervalos.push(parseFloat((ultimoElemento+0.11).toFixed(2)))
-        }
+function subtractFee(input){
+  let intervals = [0.02, 0.21, 0.32, 0.43];
+  let fees = [0.02, 0.03, 0.04, 0.05, 0.07, 0.09];
+
+  while (input > intervals[intervals.length - 1]) {
+      const lastElement = intervals[intervals.length - 1];
+      if (intervals.length % 2 === 0) {
+          intervals.push(parseFloat((lastElement + 0.12).toFixed(2)));
+      } else {
+          intervals.push(parseFloat((lastElement + 0.11).toFixed(2)));
       }
-      while(intervalos.length > taxas.length){
-        const ultimoElemento = taxas[taxas.length -1]
-        if (taxas.length % 2 === 0) {
-         taxas.push(parseFloat((ultimoElemento+0.01).toFixed(2)))
-        } else {
-          taxas.push(parseFloat((ultimoElemento+0.02).toFixed(2)))
-        }
+  }
+  while (intervals.length > fees.length) {
+      const lastElement = fees[fees.length - 1];
+      if (fees.length % 2 === 0) {
+          fees.push(parseFloat((lastElement + 0.01).toFixed(2)));
+      } else {
+          fees.push(parseFloat((lastElement + 0.02).toFixed(2)));
       }
-  
-    const primeiroMaior = intervalos.find((valor) => valor >= entrada)
-    const indicePrimeiroMaior = intervalos.indexOf(primeiroMaior)
-    const taxaSubtracao = parseFloat((entrada - taxas[indicePrimeiroMaior-1]).toFixed(2))
-  
-      console.log(entrada,taxaSubtracao)
-      return taxaSubtracao
-    }
-    
-  
-  function somaTaxa(entrada){
-      let intervalos =[0.19]
-      let taxas = [0.02,0.03,0.04,0.06,0.07,0.09]
-  
-      while(entrada > intervalos[intervalos.length -1]){
-        intervalos.push(parseFloat((intervalos[intervalos.length -1]+0.1).toFixed(2)))
+  }
+
+  const firstGreater = intervals.find((value) => value >= input);
+  const indexOfFirstGreater = intervals.indexOf(firstGreater);
+  const feeSubtraction = parseFloat((input - fees[indexOfFirstGreater - 1]).toFixed(2));
+
+  console.log("subtractFee:","input:",input,"input - fee:", feeSubtraction);
+  return feeSubtraction;
+}
+
+function sumFee(input){
+  let intervals = [0.19];
+  let fees = [0.02, 0.03, 0.04, 0.06, 0.07, 0.09];
+
+  while (input > intervals[intervals.length - 1]) {
+      intervals.push(parseFloat((intervals[intervals.length - 1] + 0.1).toFixed(2)));
+  }
+  while (intervals.length > fees.length) {
+      const lastElement = fees[fees.length - 1];
+      if (fees.length % 2 === 0) {
+          fees.push(parseFloat((lastElement + 0.01).toFixed(2)));
+      } else {
+          fees.push(parseFloat((lastElement + 0.02).toFixed(2)));
       }
-      while(intervalos.length > taxas.length){
-        const ultimoElemento = taxas[taxas.length -1]
-        if (taxas.length % 2 === 0) {
-         taxas.push(parseFloat((ultimoElemento+0.01).toFixed(2)))
-        } else {
-          taxas.push(parseFloat((ultimoElemento+0.02).toFixed(2)))
-        }
-      }
-      const indiceUltimoElemento = intervalos.indexOf(intervalos[intervalos.length -1])
-      const taxaSoma = parseFloat((entrada+taxas[indiceUltimoElemento]).toFixed(2))
-      console.log(entrada,taxaSoma)
-      return taxaSoma
-    }
-    const number = 0.31
-    somaTaxa(number)
-    subtraiTaxa(number)
+  }
+  const indexOfLastElement = intervals.indexOf(intervals[intervals.length - 1]);
+  const feeSum = parseFloat((input + fees[indexOfLastElement]).toFixed(2));
+  const fee = feeSum - input;
+  console.log("sumFee:","input:",input, "input+fee:",feeSum);
+  return feeSum;
+}
+
+const number = 0.31;
+sumFee(number);
+subtractFee(number);
